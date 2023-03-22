bc9135a0222b059f0095490ee06ce23000808dd4
silvialep <silvia.lepore_sl@libero.it>
2023-03-22 14:56:27 +0100

    feat: printed on page 10 addresses

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati
+tutti generati.
+*/
+
+
+
+
+
+
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: '',
+            emails: [],
+
         }
-    }
+    },
+
+    
+
+
+    created() {
+        for(i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+            console.log(res.data.response);
+            this.message = res.data.response;
+            this.emails.push(this.message);
+            })
+        
+        }
+    },
+
+
 }).mount('#app')
